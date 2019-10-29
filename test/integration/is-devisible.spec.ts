6eed8a71ea14d7798e2e9e6ac4b4fff26c6db585
Lakitna <lakitna@gmail.com>
2019-10-29 15:44:05 +0100

    Restructured existing integration tests

--- test/integration/is-devisible.spec.ts
+++ test/integration/is-devisible.spec.ts
@@ -2,16 +2,19 @@ import { expect } from 'chai';
 import { Lawbook } from '../../src/lawbook';
 import law from './is-devisible';
 
-describe(`Law: is-devisible`, function() {
-    beforeEach(function(this: any) {
+const lawName = 'is-devisible';
+
+describe(`Law: ${lawName}`, function() {
+    beforeEach(async function(this: any) {
         this.book = new Lawbook({
             laws: {
-                'is-devisible': {
+                [lawName]: {
                     required: 'must',
                 },
             },
         });
-        law(this.book);
+        await law(this.book);
+        this.law = this.book.filter(lawName).laws[0];
     });
 
     it('has a default config', function() {
@@ -25,18 +28,18 @@ describe(`Law: is-devisible`, function() {
     });
 
     it('is kept when the number is cleanly devisible to an integer', async function() {
-        await this.book.enforce('*', 4, 2);
-        await this.book.enforce('*', 9, 3);
-        await this.book.enforce('*', 68452, 2);
+        await this.book.enforce(this.law.name, 4, 2);
+        await this.book.enforce(this.law.name, 9, 3);
+        await this.book.enforce(this.law.name, 68452, 2);
     });
 
     it('is broken when the number is cleanly devisible to a fraction', async function() {
-        await expect(this.book.enforce('*', 3, 2))
+        await expect(this.book.enforce(this.law.name, 3, 2))
             .to.be.rejectedWith('3 is not devisible by 2');
     });
 
     it('is broken when the number is not cleanly devisible', async function() {
-        await expect(this.book.enforce('*', 3, 1.9))
+        await expect(this.book.enforce(this.law.name, 3, 1.9))
             .to.be.rejectedWith('3 is not devisible by 1.9');
     });
 });
--- test/integration/is-power-of.spec.ts
+++ test/integration/is-power-of.spec.ts
@@ -2,10 +2,19 @@ import { expect } from 'chai';
 import { Lawbook } from '../../src/lawbook';
 import law from './is-power-of';
 
-describe(`Law: is-power-of`, function() {
-    beforeEach(function(this: any) {
-        this.book = new Lawbook();
-        law(this.book);
+const lawName = 'is-power-of';
+
+describe(`Law: ${lawName}`, function() {
+    beforeEach(async function(this: any) {
+        this.book = new Lawbook({
+            laws: {
+                [lawName]: {
+                    required: 'must',
+                },
+            },
+        });
+        await law(this.book);
+        this.law = this.book.filter(lawName).laws[0];
     });
 
     it('has the root default config', function() {
@@ -18,13 +27,13 @@ describe(`Law: is-power-of`, function() {
     });
 
     it('is kept when the number is a power of', async function() {
-        await this.book.enforce('*', 4, 2);
-        await this.book.enforce('*', 9, 2);
-        await this.book.enforce('*', 27, 3);
+        await this.book.enforce(this.law.name, 4, 2);
+        await this.book.enforce(this.law.name, 9, 2);
+        await this.book.enforce(this.law.name, 27, 3);
     });
 
     it('is broken when the number is not a power of', async function() {
-        await expect(this.book.enforce('*', 3, 1.9))
+        await expect(this.book.enforce(this.law.name, 3, 1.9))
             .to.be.rejectedWith('3 is not a power of 1.9');
     });
 });
