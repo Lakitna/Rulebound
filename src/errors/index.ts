4c45a7660cc113cc8fbb65b22daad5642b2b3f91
Lakitna <lakitna@gmail.com>
2022-12-13 09:33:57 +0100

    Removed throw on UnhandledPromiseRejectionWarning. This is now default behaviour

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,8 +1,8 @@
 /* istanbul ignore file */
 
-export { RulebookError } from './rulebook-error';
-export { RuleError } from './rule-error';
 export { ConfigError } from './config-error';
+export { RuleError } from './rule-error';
+export { RulebookError } from './rulebook-error';
 
 /**
  * Limit unwieldly stack traces, but only do so if it's not limited
@@ -11,15 +11,3 @@ export { ConfigError } from './config-error';
 if (Error.stackTraceLimit > 25) {
     Error.stackTraceLimit = 25;
 }
-
-process.on('unhandledRejection', (error: any) => {
-    /**
-     * Get rid of the `UnhandledPromiseRejectionWarning` when throwing
-     * an error in a promise, because it really should behave like a
-     * normal error does.
-     *
-     * Sadly this will affect all unhandled rejections, not just the ones
-     * thrown by rules.
-     */
-    throw error;
-});
