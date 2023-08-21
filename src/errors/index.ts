52b1ce0c90fc354886e673b726b4df0d2c4b0165
Lakitna <lakitna@gmail.com>
2023-08-21 12:49:55 +0200

    No longer limiting stacktrace length
    We should not do such things here in Rulebound

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,13 +1,3 @@
-/* istanbul ignore file */
-
 export { ConfigError } from './config-error';
 export { RuleError } from './rule-error';
 export { RulebookError } from './rulebook-error';
-
-/**
- * Limit unwieldly stack traces, but only do so if it's not limited
- * more rigorously by another package.
- */
-if (Error.stackTraceLimit > 25) {
-    Error.stackTraceLimit = 25;
-}
